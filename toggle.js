07e25fd35de53b7edc79c37975e16988330981ce
harrison-zhu <harrison0zhu@gmail.com>
2025-09-17 22:46:57 -0500

    Altered layout on sunset.css

--- toggle.js
+++ toggle.js
@@ -1,9 +1,27 @@
+// function professional_style() {
+//     document.getElementById('page_style').setAttribute("href", "professional.css");
+// }
+
+// function sunset_style() {
+//     document.getElementById('page_style').setAttribute("href", "sunset.css");
+// }
+
+function load_style() {
+    page_style = localStorage.getItem("page_stylesheet_name");
+    if (page_style === null) {
+        page_style = "professional.css";
+    }
+    document.getElementById('page_style').setAttribute("href", page_style);
+}
+
 function professional_style() {
-    document.getElementById('page_style').setAttribute("href", "professional.css");
+    localStorage.setItem("page_stylesheet_name", "professional.css");
+    load_style();
 }
 
 function sunset_style() {
-    document.getElementById('page_style').setAttribute("href", "sunset.css");
+    localStorage.setItem("page_stylesheet_name", "sunset.css");
+    load_style();
 }
 
 // function load_style() {
@@ -11,17 +29,8 @@ function sunset_style() {
 //     if (page_style === null) {
 //         page_style = "professional.css";
 //     }
+//     page_style = null;
 //     document.getElementById('page_style').setAttribute("href", page_style);
 // }
 
-// function professional_style() {
-//     localStorage.setItem("page_stylesheet_name", "professional.css");
-//     load_style();
-// }
-
-// function sunset_style() {
-//     localStorage.setItem("page_stylesheet_name", "sunset.css");
-//     load_style();
-// }
-
-// load_style() // load style immediately
+load_style() // load style immediately
